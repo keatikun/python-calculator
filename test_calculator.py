6e156204b5e40c52dc0167fffcb86db4c7e8f7f1
keatikun <143638295+keatikun@users.noreply.github.com>
2024-11-07 22:14:54 +0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,40 @@ class TestCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+    def testadd(self):
+        self.assertEqual(self.calc.add(1, 2), 3) # acctual output vs. expected output
 
     # Add the following test methods to the TestCalculator class:
+    def testaddnegative(self):
+        self.assertEqual(self.calc.add(-1,-2),-3)
 
-if __name__ == '__main__':
+    def testadd_zero(self):
+        self.assertEqual(self.calc.add(1,0),1)
+
+    def test_subtract(self):
+        self.assertAlmostEqual(self.calc.subtract(5,2),3)
+
+    def test_subtract_2(self):
+        self.assertAlmostEqual(self.calc.subtract(-4,-5),1)
+
+    def test_multiply(self):
+        self.assertAlmostEqual(self.calc.multiply(-4,-5),20)
+
+    def test_multiply_2(self):
+        self.assertAlmostEqual(self.calc.multiply(50,0),0)
+
+    def test_divide(self):
+        self.assertAlmostEqual(self.calc.divide(50,25),2)
+
+    def test_divide_2(self):
+        self.assertAlmostEqual(self.calc.divide(50,1),50)
+
+    def test_modulo(self):
+        self.assertAlmostEqual(self.calc.modulo(5,2),1)
+
+    def test_modulo_2(self):
+        self.assertAlmostEqual(self.calc.modulo(10,4),2)
+
+
+if __name == '__main':
     unittest.main()
